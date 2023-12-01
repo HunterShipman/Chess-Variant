d259c1f2f1aaad95cbd68f942b30937987a8b355
HunterShipman <huntershipman15@gmail.com>
2023-11-30 16:53:13 -0800

    Wrote the ChessPiece class and its children. have not written the functions that determine if a pieces attempted move is legal or not.

--- ChessVar.py
+++ ChessVar.py
@@ -1,83 +1,138 @@
 # Author: Hunter Shipman
 # GitHub username: HunterShipman
-# Date: 11/29/2023
-# Description:
+# Date: 11/30/2023
+# Description:  A ChessVar class that will allow two players to play a derivative of chess. In this version of chess, a
+#               player wins by taking all of one type of chess piece. For example, you can win by taking both knights,
+#               every single pawn, or just the queen. Also, the king is not a special piece, it still moves like a king,
+#               but it cannot castle and there is no check or checkmate. Also, pawns cannot use en passant or pawn
+#               promotion, however they can still move two places forward on their first move and still capture
+#               diagonally.
 
 class ChessVar:
     """
     This class will be used to play the (derivative) game of chess.
 
-    It will communicate with the ChessPiece class (and it's children classes) to determine different aspects of a
+    It will communicate with the children of the ChessPiece class to determine different aspects of a
     specific chess piece, such as what pieces each piece is allowed to capture and what moves it is allowed to make.
     """
     def __init__(self, w_dict, b_dict, game_state, turn, board):
-        """
-        DETAILED TEXT DESCRIPTIONS OF HOW TO HANDLE....
-
-        1. Initializing the ChessVar class
-        2. keeping track of turn order.
-        3. keeping track of the current board position
-
-            w_dict: a dictionary containing all the pieces player white has on the board. ["piece_name"] = x amount left
-            b_dict: same as w_dict but for player black
-                NOTE: I will use these dictionaries to determine once a player has won the game. Once a player makes a
-                move that captures a chess piece, i will decrement the count of that chess piece. then I will check if
-                doing so decremented a chess pieces count to 0, at which point the player has won.
-
-            game_state: initialize to "UNFINISHED", will change to indicate once a player has won
-
-        2. turn: will initialize to "WHITE" to indicate it is player white's turn and then be toggled to "BLACK" once
-                 player white has moved (will be done by make_move method).
-
-        3. board: This will represent the chess board. It will be a list of lists. each list will represent a row on the
-                  chess board, and each lists index will represent a square in that row.
-        """
-
         pass
 
     def get_game_state(self):
         """
-        just returns the chess boards game state data member
+        returns the chess boards game state data member
         """
         pass
 
     def make_move(self, start, finish):
         """
-        DETAILED TEXT DESCRIPTIONS OF HOW TO HANDLE....
 
-        4. Determining if a regular move is valid
-        5. Determining if a capture is valid
-        6. Determining the current state of the game
-
-            this method will take the square to move from and square to move to as a string. The method will check if a
-            player has already won, if the move is illegal, if the square to move from contains the wrong players chess
-            piece and return False if any of these occur.
-
-
-        4. I plan on splitting each parameter string into an x and y. the x will be a character. I will then convert the
-            x into a pseudo ascii number. (for example if i have a3, i will turn that into "a" and "3". then I will turn
-            "a" into probably 1. so now I have 1 and 3.) Once I have done this with the finish position, I plan on
-            subtracting the starting positions x from the finished positions x to get the change of x. Then I will pull
-            the piece that is attempting to move's allowed change in x and compare the attempted move to this. If the
-            piece is allowed to move, i will then to the same for the change in y to determine if the move is legal.
-
-        5. if the move is legal I will then determine if the capture is valid. I plan on doing this by comparing what
-            pieces the "capturing" piece is allowed to capture (will probably be in a list) to the piece that is in the
-            square that the capturing piece is attempting to move to. if the capturing piece is attempting to move into a
-            square that has a piece it is not allowed to capture, I will return FALSE.
-
-        6. If the move is allowed to be made, the method will move the chess piece, remove any captured piece from the
-            opponents dictionary, update the game state if necessary (if the piece removed from the board updates that
-            specific piece's count on the board to 0), update the player_turn data member, and return true.
         """
-
         pass
 
 
 class ChessPiece:
     """
-    This class will act as a parent class for each different chess piece. I haven't figured out the specifics yet but
-    there will be a child class for each chess piece necessary. Each chess piece class will have data members for the
-    pieces name, what move's it is allowed to make, and what pieces it is allowed to capture. the only methods each
-    class will have are get_methods for its data members.
+    This class will act as a parent class for each different chess piece. Each child class will have a different "name"
+    that is one letter. If the piece's color is Black, said letter will be uppercase. For example the black kings name
+    will be "K", and the white king will be "k".
+
+    The point of having this parent class is to not have to write each get method for the child classes. Also, because
+    each piece has different allowed moves, the is_move_legal method will work differently for each class but will
+    always return True or False. This allows us to use polymorphism when checking if a pieces attempted move is legal.
+
     """
+    def __init__(self, color):
+        self._name = None  # will be overriden by child
+        self._has_moved = False  # Once a piece has moved, changes to True
+        self._color = color  # must be "WHITE" or "BLACK"
+
+    def get_has_moved(self):
+        """
+        returns True if the piece has moved and False if it hasn't
+        """
+        return self._has_moved
+
+    def set_has_moved(self):
+        """
+        sets the objects has_moved status to true
+        """
+        self._has_moved = True
+
+    def get_color(self):
+        """
+        returns the color data member of the piece
+        """
+        return self._color
+
+    def get_name(self):
+        return self._name
+
+
+class King(ChessPiece):
+    """
+    A King class that is a child of the ChessPiece class. Used to represent the king piece and it's attributes.
+
+    MOVES: can move one square in any direction. like a furnace in minecraft.
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "K" if color == "BLACK" else "k"
+
+
+class Queen(ChessPiece):
+    """
+    A Queen class that is a child of the ChessPiece class. Used to represent the Queen piece and it's attributes.
+
+    MOVES: straight up and down OR diagonally. as far as she wants
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "Q" if color == "BLACK" else "q"
+
+
+class Rook(ChessPiece):
+    """
+    A Rook class that is a child of the ChessPiece class. Used to represent a Rook piece and it's attributes.
+
+    MOVES: Straight up and down or left to right. as far as he wants
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "R" if color == "BLACK" else "r"
+
+
+class Bishop(ChessPiece):
+    """
+    A Bishop class that is a child of the ChessPiece class. Used to represent a Bishop piece and it's attributes.
+
+    MOVES: only diagonally. as far as he wants
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "B" if color == "BLACK" else "b"
+
+
+class Knight(ChessPiece):
+    """
+    A Knight class that is a child of the ChessPiece class. Used to represent a Knight piece and it's attributes.
+
+    MOVES: "like a capital L". Two squares one direction, one to the side. EX: two down one left.
+        NOTE: can "jump" over another piece.
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "N" if color == "BLACK" else "n"
+
+
+class Pawn(ChessPiece):
+    """
+    A Pawn class that is a child of the ChessPiece class. Used to represent a Pawn piece and it's attributes.
+
+    MOVES: Can move one square forward, never backward.
+        NOTE: on first move, can move two or one squares forward.
+        NOTE: ONLY captures diagonally. cannot capture if the square in front is occupied
+    """
+    def __init__(self, color):
+        super().__init__(color)
+        self._name = "P" if color == "BLACK" else "p"
